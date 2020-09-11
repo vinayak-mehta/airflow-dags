e12a01ef6bd76d6d27d1da8ccc82861dadeeaf85
Vinayak Mehta <vmehta94@gmail.com>
2020-09-12 03:09:09 +0530

    Remove old dag

--- dags/notebook_v1.py
+++ dags/notebook_v1.py
@@ -1,24 +1,23 @@
 # -*- coding: utf-8 -*-
 
-import datetime as dt
-
-from airflow import DAG
+from airflow.models import DAG
+from airflow.utils.dates import days_ago
+from airflow.operators.papermill_operator import PapermillOperator
 
 
 args = {
     "owner": "vinayak",
-    "start_date": dt.datetime.strptime("2020-09-12T00:00:00", "%Y-%m-%dT%H:%M:%S"),
+    "start_date": days_ago(1),
 }
 
-dag = DAG(
+with DAG(
     dag_id="notebook_v1",
-    default_args=args,
-    schedule_interval="*/10 * * * *",
-)
-
-notebook_task = PapermillOperator(
-    task_id="run_notebook",
-    input_nb="/opt/airflow/dags/notebook.ipynb",
-    output_nb="/tmp/{{ dag.dag_id }}/{{ run_id }}/notebook.ipynb",
-    dag=dag,
-)
+    default_args=default_args,
+    schedule_interval="0 0 * * *",
+) as dag:
+    run_this = PapermillOperator(
+        task_id="run_example_notebook",
+        input_nb="/opt/airflow/dags/notebook.ipynb",
+        output_nb="/tmp/{{ dag.dag_id }}/{{ run_id }}/notebook.ipynb",
+        parameters={"msgs": "Ran from Airflow at {{ execution_date }}!"},
+    )
